415abef5bef334eeeaa0cd1400c9e49490e9a14f
Erik Rimskog <erik.rimskog@gmail.com>
2023-07-26 14:45:55 +0200

    stuff

--- keyboards/moonlander/keymaps/eriks/keymap.c
+++ keyboards/moonlander/keymaps/eriks/keymap.c
@@ -27,6 +27,7 @@ enum layers {
 #define MOON_LED_LEVEL LED_LEVEL
 
 #define CTL_ESC MT(MOD_LCTL, KC_ESCAPE)
+#define ALT_TAB MT(MOD_LALT, KC_TAB)
 #define OSM_SHFT OSM(MOD_LSFT)
 #define TD_CTLSH TD(TD_CTRL_SHIFT)
 #define TD_SUPSH TD(TD_SUPER_SHIFT)
@@ -34,11 +35,11 @@ enum layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_bas] = LAYOUT_moonlander(
     KC_TRNS,   KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_PSCREEN,
-    KC_TAB,    KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_TRNS,        KC_TRNS, KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    KC_BSPACE,
+    ALT_TAB,   KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_TRNS,        KC_TRNS, KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    KC_BSPACE,
     CTL_ESC,   KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_TRNS,        KC_TRNS, KC_H,    KC_J,    KC_K,     KC_L,    SE_APOS, KC_ENTER,
     OSM_SHFT,  KC_Z,     KC_X,    KC_C,    KC_V,    KC_B,                             KC_N,    KC_M,    KC_COMMA, KC_DOT,  SE_MINS, TG_SHIFT,
-    KC_TRNS,   KC_TRNS,  KC_TRNS, TT(_sym),TT(_num),         TG(_sys),       KC_TRNS,          TT(_sym),TT(_num), KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_BSPACE, TD_CTLSH, TD_SUPSH,                                                                                KC_RGUI, KC_LALT, KC_SPACE
+    KC_TRNS,   KC_TRNS,  KC_TRNS, TT(_sym),TT(_num),         KC_TRNS,        KC_TRNS,          TT(_sym),TT(_num), KC_TRNS, KC_TRNS, KC_TRNS,
+    TD_CTLSH,  TD_SUPSH, KC_TRNS,                          /*^TG(_gaming)^*/                                      KC_TRNS, KC_LALT, KC_SPACE
   ),
   [_sym] = LAYOUT_moonlander(
     KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,
@@ -46,14 +47,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     TO(_bas),  KC_TRNS, SE_AT,   SE_LBRC, SE_RBRC, SE_AMPR, KC_CAPSLOCK,     KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, SE_QUES, KC_TRNS,
     KC_TRNS,   KC_TRNS, SE_TILD, SE_LCBR, SE_RCBR, SE_CIRC,                           SE_PLUS, SE_UNDS, SE_LESS, SE_GRTR,  KC_TRNS, KC_TRNS,
     KC_TRNS,   KC_TRNS, KC_TRNS, TG(_sym),TO(_mus),         KC_NO,           TO(_bas),         TG(_sym),KC_NO,   KC_TRNS,  KC_TRNS, KC_TRNS,
-    KC_DELETE, KC_TRNS, KC_TRNS,                                                                                 KC_TRNS,  KC_TRNS, KC_TRNS
+    KC_TRNS,   KC_TRNS, KC_TRNS,                                                                                 KC_TRNS,  KC_TRNS, KC_TRNS
   ),
   [_num] = LAYOUT_moonlander(
     KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_NUMLOCK, KC_TRNS, SE_GRV,  SE_ACUT, KC_TRNS, KC_TRNS,        KC_TRNS, SE_SLSH, KC_7,    KC_8,    KC_9,    SE_PLUS, KC_TRNS,
-    TO(_bas),KC_TRNS,    SE_AA,   SE_ADIA, SE_OSLH, KC_TRNS, KC_TRNS,        KC_TRNS, SE_ASTR, KC_4,    KC_5,    KC_6,    SE_EQL,  KC_TRNS,
-    KC_TRNS, KC_TRNS,    SE_PIPE, SE_SECT, SE_MU,   KC_TRNS,                          SE_MINS, KC_1,    KC_2,    KC_3,    KC_DOT,  KC_TRNS,
-    KC_TRNS, KC_TRNS,    KC_TRNS, KC_NO,   TG(_num),         KC_NO,          TO(_bas),         TO(_mus),TG(_num),KC_TRNS, KC_TRNS, KC_TRNS,
+    TO(_bas),KC_TRNS,    SE_AA,   SE_ADIA, SE_OSLH, KC_TRNS, KC_TRNS,        KC_TRNS, KC_COMMA,KC_4,    KC_5,    KC_6,    SE_ASTR, KC_TRNS,
+    KC_TRNS, KC_TRNS,    SE_PIPE, SE_SECT, SE_MU,   KC_TRNS,                          KC_DOT,  KC_1,    KC_2,    KC_3,    SE_UNDS, KC_TRNS,
+    KC_TRNS, KC_TRNS,    KC_TRNS, KC_NO,   TG(_num),         KC_NO,          TO(_bas),         TO(_sys),TG(_num),KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS, KC_TRNS,    KC_TRNS,                                                                                KC_TRNS, KC_TRNS, KC_0
   ),
   //NOTE: not in use
@@ -87,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,   RGB_VAI,RGB_HUI,           RGB_SPI,RGB_SAI,KC_NO,         KC_NO,         KC_NO, KC_NO, KC_NO,KC_NO,AU_TOG, MU_TOG, MU_MOD,
     TO(_bas),RGB_VAD,RGB_HUD,           RGB_SPD,RGB_SAD,KC_NO,         KC_NO,         KC_NO, KC_NO, KC_NO,KC_NO,KC_NO,  DT_UP,  RESET,
     KC_NO,   RGB_SLD,TOGGLE_LAYER_COLOR,RGB_MOD,RGB_TOG,MOON_LED_LEVEL,                      KC_NO, KC_NO,KC_NO,DT_PRNT,DT_DOWN,MAGIC_TOGGLE_NKRO,
-    KC_NO,   KC_NO,  KC_NO,             KC_NO,  KC_NO,                 KC_TRNS,       TO(_bas),     KC_NO,KC_NO,KC_NO,  KC_NO,  KC_NO,
+    KC_NO,   KC_NO,  KC_NO,             KC_NO,  KC_NO,                 KC_NO,         TO(_bas),     KC_NO,KC_NO,KC_NO,  KC_NO,  KC_NO,
     KC_NO,   KC_NO,  KC_NO,                                                                                     KC_NO,  KC_NO,  KC_NO
   ),
 };
@@ -109,12 +110,12 @@ void keyboard_post_init_user(void) {
 #define MAGENTA {216,247,255}
 
 const uint8_t PROGMEM ledmap[][DRIVER_LED_TOTAL][3] = {
-    [_sym] = { ____, ____, WHITE, ____, ____, ____, BLUE, ____, ____, ____, ____, BLUE, YELLOW, YELLOW, ____, ____, BLUE, HOTPINK, HOTPINK, ____, ____, BLUE, HOTPINK, HOTPINK, CYAN, ____, BLUE, BLUE, YELLOW, ____, WHITE, RED, GREEN, ____, ____, ____, ____, GREEN, ____, ____, ____, ____, BLUE, CYAN, ____, ____, ____, BLUE, HOTPINK, YELLOW, ____, ____, BLUE, HOTPINK, YELLOW, ____, ____, BLUE, HOTPINK, YELLOW, ____, ____, BLUE, HOTPINK, YELLOW, ____, ____, ____, ____, ____, ____, WHITE },
+    [_sym] = { ____, ____, WHITE, ____, ____, ____, BLUE, ____, ____, ____, ____, BLUE, YELLOW, YELLOW, ____, ____, BLUE, HOTPINK, HOTPINK, ____, ____, BLUE, HOTPINK, HOTPINK, CYAN, ____, BLUE, BLUE, YELLOW, ____, WHITE, RED, ____, ____, ____, ____, ____, GREEN, ____, ____, ____, ____, BLUE, CYAN, ____, ____, ____, BLUE, HOTPINK, YELLOW, ____, ____, BLUE, HOTPINK, YELLOW, ____, ____, BLUE, HOTPINK, YELLOW, ____, ____, BLUE, HOTPINK, YELLOW, ____, ____, ____, ____, ____, ____, WHITE },
     [_num] = { ____, ____, WHITE, ____, ____, ____, RED, ____, ____, ____, ____, ____, HOTPINK, YELLOW, ____, ____, CYAN, HOTPINK, YELLOW, ____, ____, CYAN, HOTPINK, YELLOW, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, BLUE, BLUE, BLUE, ____, ____, MAGENTA, MAGENTA, MAGENTA, ____, ____, MAGENTA, MAGENTA, MAGENTA, ____, ____, MAGENTA, MAGENTA, MAGENTA, CYAN, ____, BLUE, BLUE, BLUE, ____, ____, ____, MAGENTA, ____, ____, WHITE },
     [_lmod] = { ____, CYAN, ____, CYAN, ____, ____, CYAN, HOTPINK, CYAN, ____, ____, CYAN, HOTPINK, CYAN, ____, ____, CYAN, HOTPINK, CYAN, ____, ____, CYAN, HOTPINK, CYAN, ____, ____, CYAN, HOTPINK, CYAN, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, BLUE, ____, ____, ____, ____, GREEN, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, WHITE },
     [_rmod] = { ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, GREEN, ____, ____, ____, ____, BLUE, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, CYAN, ____, CYAN, ____, ____, CYAN, HOTPINK, CYAN, ____, ____, CYAN, HOTPINK, CYAN, ____, ____, CYAN, HOTPINK, CYAN, ____, ____, CYAN, HOTPINK, CYAN, ____, ____, CYAN, HOTPINK, CYAN, ____, ____, ____, ____, ____, ____, WHITE },
     [_mus] = { ____, ____, WHITE, ____, ____, ____, ____, ____, ____, ____, ____, HOTPINK, HOTPINK, ____, ____, ____, HOTPINK, HOTPINK, ____, WHITE, ____, HOTPINK, HOTPINK, ____, WHITE, ____, ____, ____, ____, ____, ____, ____, HOTPINK, ____, ____, ____, ____, ____, ____, ____, ____, ____, YELLOW, YELLOW, YELLOW, ____, ____, MAGENTA, RED, CYAN, ____, ____, MAGENTA, RED, CYAN, WHITE, ____, MAGENTA, RED, CYAN, WHITE, ____, CYAN, CYAN, CYAN, ____, ____, ____, ____, ____, ____, WHITE },
-    [_sys] = { ____, ____, WHITE, ____, ____, ____, GREEN, GREEN, GREEN, ____, ____, GREEN, GREEN, GREEN, ____, ____, GREEN, GREEN, GREEN, ____, ____, GREEN, GREEN, GREEN, ____, ____, ____, ____, GREEN, ____, ____, ____, ____, ____, ____, BLUE, ____, YELLOW, RED, HOTPINK, ____, ____, YELLOW, CYAN, CYAN, ____, ____, YELLOW, ____, CYAN, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, WHITE },
+    [_sys] = { ____, ____, WHITE, ____, ____, ____, GREEN, GREEN, GREEN, ____, ____, GREEN, GREEN, GREEN, ____, ____, GREEN, GREEN, GREEN, ____, ____, GREEN, GREEN, GREEN, ____, ____, ____, ____, GREEN, ____, ____, ____, ____, ____, ____, ____, ____, YELLOW, RED, HOTPINK, ____, ____, YELLOW, CYAN, CYAN, ____, ____, YELLOW, ____, CYAN, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, ____, WHITE },
 };
 
 void set_layer_color(int layer) {
@@ -146,17 +147,12 @@ void rgb_matrix_indicators_user(void) {
 
 static bool tg_shift_active = false;
 
-void update_tg_shift_led(void) {
+static void update_tg_shift_led(void) {
   ML_LED_5(tg_shift_active);
 }
 
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-  case RGB_SLD:
-    if (record->event.pressed) {
-      rgblight_mode(1);
-    }
-    return false;
+static bool process_record_toggle_shift(uint16_t keycode, keyrecord_t *record) {
+  switch(keycode) {
   case TG_SHIFT:
     if (!record->event.pressed) return false;
     if (tg_shift_active) {
@@ -167,9 +163,16 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     tg_shift_active = !tg_shift_active;
     update_tg_shift_led();
     return false;
-  case KC_SPACE:
-  case KC_ENTER:
-  case CTL_ESC:
+  case KC_A ... KC_Z:
+  case SE_MINS:
+  case SE_UNDS:
+  case KC_BSPACE:
+  case TT(_num): // where åäö are
+  case SE_AA:
+  case SE_ADIA:
+  case SE_OSLH:
+    return true;
+  default:
     if (tg_shift_active) {
       tg_shift_active = false;
       unregister_code(KC_LSHIFT);
@@ -177,7 +180,21 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
   }
+}
+
+static bool process_record_ML(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+  case RGB_SLD:
+    if (record->event.pressed) {
+      rgblight_mode(1);
+    }
+    return false;
+  }
   return true;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  return process_record_ML(keycode, record) && process_record_toggle_shift(keycode, record);
 }
 
 void td_ctrl_shift_each(qk_tap_dance_state_t *state, void *user_data) {
